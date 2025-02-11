723f428a42034e437365f7d95668929907e56b40
Giancarlo Pagliarini Alves <giancarlopalvess@outlook.com>
2025-02-11 20:26:43 -0300

    first commit

---
+++ src/main/java/dev/giancarlo/cyborgdata/CyborgdataApplication.java
@@ -0,0 +1,13 @@
+package dev.giancarlo.cyborgdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyborgdataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CyborgdataApplication.class, args);
+	}
+
+}
